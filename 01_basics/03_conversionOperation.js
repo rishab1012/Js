70d5b12fd7e55600e07534387275462e9cbc55ed
rishab1012 <96413419+rishab1012@users.noreply.github.com>
2024-02-16 11:09:02 +0000

    conatiner config done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,8 +3,8 @@ let score = "Rishab"
 // console.log(typeof score); // two ways to check the data type 
 // console.log(typeof(score)); // we check the type we are not sure which format it is 
 
-let valueinNumber = Number(score)
-console.log(typeof valueinNumber);
+// let valueinNumber = Number(score)
+// console.log(typeof valueinNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -29,5 +29,43 @@ let valuesomeUserName = String(someUserName)
 
 let myFriendName ="Johaan"
 
-let convertmyFriendName = Number(myFriendName)
-console.log(typeof convertmyFriendName);
+// let convertmyFriendName = Number(myFriendName)
+// console.log(typeof convertmyFriendName);
+
+// **************** Operation**********
+
+let value = 3
+let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// // let str2 = " rishab"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 );
+// console.log(1 + "2");
+// console.log(1 + +2 +"2");
+// console.log("1" + +2 +2);
+
+// console.log((3 + 4) * 5 % 3 );
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2 + 2
+// console.log(num1);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
