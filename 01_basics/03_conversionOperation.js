c965af9a94c1e64365a3d3a24941bb354b4c98e1
rishab1012 <96413419+rishab1012@users.noreply.github.com>
2024-02-16 10:29:04 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Rishab"
+
+// console.log(typeof score); // two ways to check the data type 
+// console.log(typeof(score)); // we check the type we are not sure which format it is 
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "rishab"
+
+let booleanisLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+
+// 1 => true; 0 => false 
+// "" => false 
+// "rishab" => true 
+
+let someUserName = 56
+
+let valuesomeUserName = String(someUserName)
+// console.log( valuesomeUserName)
+
+
+let myFriendName ="Johaan"
+
+let convertmyFriendName = Number(myFriendName)
+console.log(typeof convertmyFriendName);
