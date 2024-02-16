418e149f3c227af0214c408c38c445305691eda7
rishab1012 <96413419+rishab1012@users.noreply.github.com>
2024-02-16 06:41:21 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const userName ="Rishab"
+let userId = 10
+var userEmail = "rishab@gmail.com"
+let userState;
+userCity = "Vasco"
+
+userId = 20
+
+console.log(userName);
+console.log(userEmail);
+console.table([userName,userEmail,userId,userState,userCity])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //Treat all js code as newer version
+
+// alert(3 +3) //we are using node.js, not browser
+
+let name = "rishab"
+let age = 20
+let isLoggedIn =true
+
+//number => 2 to power of 53
+// bigInt 
+// string can be used in '' and "" better be use with ""
+// boolean  => trur / false
+// null => standlone value
+// undefined => (not defined value to varaible)
+// symbol => unique 
+
+//object
+
+console.log(typeof undefined) //undifined
+console.log(typeof null) // object
