28ae213716b92f55e9a5b56959eaa8e9e5bd2b63
rishab1012 <96413419+rishab1012@users.noreply.github.com>
2024-02-19 09:51:58 +0000

    container config done

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,35 @@
+// Dates
+
+// const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let ji = new Date(2023, 0, 2)
+// let ji = new Date(2023, 0, 2, 5, 3)
+// let ji = new Date("2023-01-14")
+let ji = new Date("01-14-2023")
+// console.log(ji.toLocaleString());
+
+let mytimeStamp =  Date.now()
+
+// console.log(mytimeStamp);
+// console.log(ji.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getHours());
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
+
+
+// do me string IntersectionObserverlsion\
+
+// console.log(newDate.toLocaleString('deafult',{
+//     weekday : "long"
+
+// }))
