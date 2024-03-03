341fcd50c05e028881fee180a39c391d0060525f
Rishab Salgaonkar <96413419+rishab1012@users.noreply.github.com>
2024-03-03 05:56:38 +0000

    Practice of 01 & 02 basics

---
+++ Practice/01_basics.js
@@ -0,0 +1,26 @@
+// 01 Basics variables 
+
+const userName ="rishab"
+let UserAge =25
+let userEmail ="rishab@yahoo.com"
+var userPhonenum =1234567890
+let DOB ;
+
+
+console.table([userName,UserAge,userEmail,userPhonenum,DOB])
+
+// 02 Basics datatypes
+
+"use strict";
+
+// Data types in javascript 
+// 1.Number
+// 2.string
+// 3.big Int
+// 4.boolean
+// 5.null
+// 6.undefined
+// 7.symbol
+// 8.objects
+
+console.log(typeof userEmail);
