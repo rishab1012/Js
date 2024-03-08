33f6ce8ba7ec8d71174ceea2953b3ecda0474fb0
Rishab Salgaonkar <96413419+rishab1012@users.noreply.github.com>
2024-03-08 13:37:34 +0000

    updated code

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,4 +37,36 @@ return
     return `${username} just logged in.`
 }
 
-console.log(loginUserMessage("rishab"));
+// console.log(loginUserMessage("rishab"));
+
+
+function calculateCartPrice(val1, valu2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,800,2000,560));
+
+const user ={
+    username:"rishab",
+    prize:599
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and prize is ${anyobject.prize}`);
+}
+
+// handleobject(user)
+
+handleobject({
+    username:"Sam",
+    prize:5999
+})
+
+const myNewArray =[200, 800, 900]
+ 
+function resturnSecondValue(getarray){
+    return getarray[1]
+}
+
+// console.log(resturnSecondValue(myNewArray));
+console.log(resturnSecondValue([100, 500, 700, 900]));
