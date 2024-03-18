13708f2722c950eaf9de77c274ced762641c3543
Rishab Salgaonkar <96413419+rishab1012@users.noreply.github.com>
2024-03-18 10:06:50 +0000

    updated code

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,51 @@ if (true){
 let a= 10
 const b= 20
 var c= 30
-console.log("Inner: ",a);
+// console.log("Inner: ",a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+     const userName ="rishab"
+
+    function two(){
+    const WebsiteName ="youtube"
+    console.log(userName);
+    }
+    // console.log(WebsiteName);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const userName ="Rishab"
+    if(userName === "Rishab"){
+        const WebsiteName = "Instagram "
+        // console.log(WebsiteName + userName)
+    }
+
+    // console.log(WebsiteName);
+}
+
+// console.log(userName);
+
+// ***************************Interesting*****************
+
+console.log(addone(5))
+
+function addone(num){
+    return num +1
+}
+
+addtwo(5) // Hoisting problem calling the variable before decalaration 
+
+const addtwo = function (num){
+    return num +2 
+}
+
