1c352979a3ed7d4a804b9c56d8733fbfbd7b3bc4
Rishab Salgaonkar <96413419+rishab1012@users.noreply.github.com>
2024-03-24 14:53:28 +0000

    Container Config Done

---
+++ 05_iterations/three.js
@@ -0,0 +1,30 @@
+// for of 
+
+// ["", "", ""] 
+// [{}, {}, {}]
+
+const arr= [1, 2, 3, 4, 5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greeting ="Hello World!"
+for (const greet of greeting) {
+    if(greet == " "){
+        continue
+    }
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps 
+
+const map = new Map()
+map.set("IN", "India")
+map.set("UK", "United Kingdom")
+map.set("FR", "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key,":-",value);
+}
