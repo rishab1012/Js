5368062dcd0f86163676eead51bb5c4b608cbc84
Rishab Salgaonkar <96413419+rishab1012@users.noreply.github.com>
2024-07-13 14:48:02 +0000

    Update

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,1 +1,32 @@
- 
+ const user = {
+    userName: "rishab",
+    loginCount: 8,
+    signedIn:true,
+
+  getUserDetails : function (){
+    // console.log("Got user details from database.");
+    // console.log(`username:${this.userName}`);
+   
+  }
+
+ }
+
+  function User(userName,loginCount,signedIn){
+     this.userName= userName
+     this.loginCount=loginCount
+     this.signedIn=signedIn
+
+     this.greeting= function(){
+       console.log(`Welcome ${this.userName}`);
+     }
+
+    return this
+     }
+  
+// const userOne =new User("rishab",5,true)
+const userTwo = new User("johan",8,true) 
+
+// console.log(userOne);
+console.log(userTwo.constructor);
+//  console.log(user.getUserDetails());
+
