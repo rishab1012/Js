d0571e87bd95be1001ed14182445d84a663558b6
Rishab Salgaonkar <96413419+rishab1012@users.noreply.github.com>
2024-07-14 11:26:25 +0000

    Container Config done

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,32 @@
+// console.log(Math.PI);
+
+let descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+ 
+// console.log(descriptor);
+
+Object.create(null)
+
+
+const chai = {
+
+    name:"rishab",
+    prof :"SD3",
+
+    getHired: function(){
+        console.log(`code phate gaya!!!!!`);
+    }
+
+}
+
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai,"name",{
+    enumerable: false
+})
+
+for (const [key, val] of Object.entries(chai)) {
+    if(typeof val !== 'function'){
+        console.log(`${key} : ${val}`);
+    }
+    
+}
